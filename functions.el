7b47e28b30aebb00e56865428732f1d51ef36e11
rafatieppo <rafaeltieppo@yahoo.com.br>
2016-02-08 06:53:36 -0300

    review .emac and functions

--- functions.el
+++ functions.el
@@ -1,4 +1,8 @@
 ;;-----------------------------------------------------------------------------
+;; Insert date
+;;-----------------------------------------------------------------------------
+
+;;-----------------------------------------------------------------------------
 ;; Funções inserir o dia e a hora no buffer.
 ;; http://stackoverflow.com/questions/251908/how-can-i-insert-current-date-and-time-into-a-file-using-emacs
 (defun today ()
@@ -8,6 +12,9 @@ e.g. Sunday, September 17, 2000."
 ;; (insert (format-time-string "%A, %e de %B , %Y"))
 (insert (format-time-string "%e/%m/%Y"))
 )
+
+;;-----------------------------------------------------------------------------
+;; Insert headerS
 ;;-----------------------------------------------------------------------------
 
 ;;-----------------------------------------------------------------------------
@@ -17,26 +24,11 @@ e.g. Sunday, September 17, 2000."
 ;; (insert (comment-dwim 2) (make-string 65 ?=) "\n")
 (insert (make-string 65 ?=) "\n")
 (insert (make-string 45 ? ) "Rafael Tieppo\n")
-(insert (make-string 45 ? ) "tiepporc@unemat.br\n")
+(insert (make-string 45 ? ) "rafaelt@unemat.br\n")
+(insert (make-string 45 ? ) "http://www2.unemat.br/rafaeltieppo/\n")
 (insert (make-string 45 ? ) (format-time-string "%d-%m-%Y\n"))
 (insert (make-string 65 ?=) "\n")
 )
-;;-----------------------------------------------------------------------------
-
-;;-----------------------------------------------------------------------------
-(defun header_org ()
-"Insere cabecalho org-mode"
-(interactive)
-(insert (make-string 0 ? ) "#+LaTeX_CLASS_OPTIONS: [article,a4paper,times,12pt] \n")
-(insert (make-string 0 ? ) "#+latex_header: \\usepackage[hmargin=2.5cm,vmargin=2.5cm]{geometry} \n")
-(insert (make-string 0 ? ) "#+LaTeX_HEADER: \\usepackage[alf,abnt-repeated-title-omit=yes,abnt-show-options=warn,abnt-emphasize=bf,abnt-etal-list=0]{abntcite} \n") 
-(insert (make-string 0 ? ) "#+LaTeX_HEADER: \\usemintedstyle{tango} \n")
-(insert (make-string 0 ? ) "#+TITLE: Title \n")
-(insert (make-string 0 ? ) "#+DATE: \n")
-(insert (make-string 0 ? ) "#+AUTHOR: Rafael Tieppo \n")
-(insert (make-string 0 ? ) "#+EMAIL: tiepporc@unemat.br \n")
-)
-;;-----------------------------------------------------------------------------
 
 ;;-----------------------------------------------------------------------------
 (defun header_md ()
@@ -60,6 +52,7 @@ e.g. Sunday, September 17, 2000."
 
 ;;-----------------------------------------------------------------------------
 ;; Insert a new (empty) chunk to R markdown.
+;;-----------------------------------------------------------------------------
 (defun insert-chunk ()
   "Insert chunk environment Rmd sessions."
   (interactive)
@@ -71,7 +64,8 @@ e.g. Sunday, September 17, 2000."
 ;;-----------------------------------------------------------------------------
 
 ;;-----------------------------------------------------------------------------
-;; Função para duplicar linhas (esse comando/atalho é muito útil).
+;; Função para duplicar linhas
+;;-----------------------------------------------------------------------------
 ;; http://stackoverflow.com/questions/88399/how-do-i-duplicate-a-whole-line-in-emacs
 ;; Define função para duplicar linhas.
 (defun duplicate-line ()
@@ -89,11 +83,12 @@ e.g. Sunday, September 17, 2000."
 ;;---------------------------------------------------------------------------
 ;; https://github.com/magnars/.emacs.d/blob/master/sane-defaults.el
 ;; Salva lista de aquivos recentes. Abrir lista com C-x f
+;;-----------------------------------------------------------------------------
 (recentf-mode 1)
 (setq recentf-max-saved-items 25) ;; 20 é muito pouco.
 ;; Salva histórico de comandos do minibuffer.
 (savehist-mode 1)
-(setq history-length 1000)
+(setq history-length 500)
 ;;---------------------------------------------------------------------------
 
 
