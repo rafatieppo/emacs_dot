d06f89c4b201f79c893f98d966002c987c9dc94b
rafatieppo <rafaeltieppo@yahoo.com.br>
2018-07-24 10:23:15 -0400

    set conda python 3 for emacs

--- EMACS_DOT.el
+++ EMACS_DOT.el
@@ -138,6 +138,10 @@
 ;(set-default-font "Anonymous Pro-14.5")
 ;(set-default-font "Menlo-13")
 (custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(default ((t (:family "Menlo" :foundry "unknown" :slant normal :weight normal :height 130 :width normal)))))
 ;;---------------------------------------------------------------------------
 ;; Space between lines and between line numbers and text (like margin)
@@ -622,8 +626,8 @@
 (global-set-key (kbd "M-/") 'python-shell-send-region-or-line) ; alt + /
 ;;-----------------------------------------------------------------------------
 ;; Set Python3 interpreter
-(setq python-shell-interpreter "/usr/bin/python3")
-
+;(setq python-shell-interpreter "/usr/bin/python3")
+(setq python-shell-interpreter "/home/rafatieppo/anaconda3/bin/python3")
 
 ;;===========================================================================
 ;; THEMES - SEVERAL SCHEMES
@@ -697,3 +701,10 @@
 ;;---------------------------------------------------------------------------
 
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(smex smartparens py-autopep8 powerline polymode multiple-cursors markdown-mode jedi indent-guide ido-vertical-mode ido-hacks highlight-symbol flx-ido ess elpy csv-mode auctex ace-jump-mode ac-math)))
