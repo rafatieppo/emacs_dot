d9fd556b20a2301b6af5dcbf95f28bd000eafdee
rafatieppo <rafaeltieppo@yahoo.com.br>
2023-04-24 20:47:11 -0400

    lsp-ui

--- 27emacs_dot.el
+++ 27emacs_dot.el
@@ -616,9 +616,22 @@
   :hook (python-mode . (lambda ()
                           (require 'lsp-pyright)
                           (lsp))))  ; or lsp-deferred 
+
+;(use-package lsp-ui
+;  :commands lsp-ui-mode)
+
+
 (use-package lsp-ui
-  :commands lsp-ui-mode)
-  
+  :config (setq lsp-ui-sideline-show-hover t
+                lsp-ui-sideline-delay 0.5
+                lsp-ui-doc-delay 5
+                lsp-ui-sideline-ignore-duplicates t
+                lsp-ui-doc-position 'bottom
+                lsp-ui-doc-alignment 'frame
+                lsp-ui-doc-header nil
+                lsp-ui-doc-include-signature t
+                lsp-ui-doc-use-childframe t))
+
 ;; pyvenv
 (use-package pyvenv
   :config
